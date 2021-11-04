5d0d70c809e89055306c3af3aa10078874482800
Siddharth Kekre <40489787+iSiddharth20@users.noreply.github.com>
2021-11-03 17:28:20 -0700

    Added Employee.java

---
+++ CS5800/Exam02/Q18_JavaCode/Employee.java
@@ -0,0 +1,13 @@
+public class Employee 
+    {
+        private int ID;
+
+        public Employee(String name, int ID)
+            {   }
+
+        public void sendMessage(String message)
+            {  
+                System.out.println(message);
+            }
+
+    }
